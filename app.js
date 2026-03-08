38921c01554d52aa6e396cb1f00acf62ec0cbdd5
Nestor Garcia <nestor_elias@unitec.edu>
2026-03-07 23:03:06 -0600

    feat: agrega endpoint PUT /api/productos/:id

--- app.js
+++ app.js
@@ -44,6 +44,86 @@ app.get('/api/productos/:id', (req, res) => {
 
 });
 
+// Obtener todos los productos
+app.get('/api/productos', (req, res) => {
+
+    const sql = "SELECT * FROM Producto";
+
+    pool.query(sql, (error, results) => {
+
+        if (error) {
+            return res.status(500).json({mensaje: "Error al obtener los productos", error: error.message});
+        }
+
+        res.status(200).json(results);
+
+    });
+
+});
+
+
+
+app.put('/api/productos/:id', (req, res) => {
+
+    const id = parseInt(req.params.id);
+    const producto = req.body;
+
+    if (isNaN(id)) {
+        return res.status(400).json({status: 400, message: 'El id debe ser un número válido'});
+    }
+
+    if (!producto.nombre || producto.nombre.trim() === '') {
+        return res.status(400).json({status: 400, message: 'El nombre es obligatorio'});
+    }
+
+    if (!producto.sku || producto.sku.trim() === '') {
+        return res.status(400).json({status: 400, message: 'El sku es obligatorio'});
+    }
+
+    if (producto.precio_compra <= 0) {
+        return res.status(400).json({status: 400,message: 'El precio_compra debe ser mayor a 0'});
+    }
+
+    if (producto.precio_venta <= 0) {
+        return res.status(400).json({status: 400, message: 'El precio_venta debe ser mayor a 0'});
+    }
+
+    if (producto.estado !== 0 && producto.estado !== 1 && producto.estado !== '0' && producto.estado !== '1') {
+        return res.status(400).json({status: 400, message: 'El estado debe ser 0 o 1'});
+    }
+
+    const sql = `
+        UPDATE producto SET nombre=?, descripcion=?, sku=?, precio_compra=?, precio_venta=?, stock_minimo=?, estado=?, id_categoria=?, id_proveedor=?
+        WHERE id_producto=?
+    `;
+
+    pool.query(sql, [
+        producto.nombre,
+        producto.descripcion,
+        producto.sku,
+        producto.precio_compra,
+        producto.precio_venta,
+        producto.stock_minimo,
+        producto.estado,
+        producto.id_categoria,
+        producto.id_proveedor,
+        id
+    ], (error, results) => {
+
+        if (error) {
+            return res.status(500).json({status: 500, message: 'Error al actualizar el producto', error: error.message});
+        }
+
+        if (results.affectedRows === 0) {
+            return res.status(404).json({status: 404, message: 'Producto no encontrado'});
+        }
+
+        res.status(200).json({status: 200, message: 'Producto actualizado correctamente'});
+
+    });
+
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
