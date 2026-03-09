0ad0bfc985f6e4da600f27a40e5b5e37aab7d640
lissybarrera24 <Lissybuezo1996@gmail.com>
2026-03-08 20:53:22 -0600

    Add DELETE endpoint for product removal

--- app.js
+++ app.js
@@ -151,6 +151,22 @@ app.post("/api/productos", (req, res) => {
   );
 });
 
+app.delete('/api/productos/:id',(req,res)=>{
+  const id = parseInt(req.params.id);
+  const sql = 'DELETE From Producto WHERE id_producto=?';
+
+  pool.query(sql,[id],(error,results)=>{
+    if(error){
+      console.log('Existe un error en la consulta SQL');
+      res.status(500).json({status:500, message:'Error en la consulta SQL...'});
+    }else if(results.affectedRows === 0){
+      res.status(404).json({status:400, message:'Producto no encontrado...'})
+    }else{
+      res.status(200).json({status:200,message:'Producto eliminado exitosamente'});
+    }
+
+  });
+})
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
